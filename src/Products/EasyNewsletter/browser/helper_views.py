5759f73b9235aa35464d8c3162a812ca30502030
Marek Kralewski <mck@tuxwerk.de>
2020-05-15 19:03:50 +0200

    removing more code that is not used...

--- src/Products/EasyNewsletter/browser/helper_views.py
+++ src/Products/EasyNewsletter/browser/helper_views.py
@@ -12,16 +12,6 @@ class IENLHelperView(Interface):
     def brain_has_lead_image(self, brain=None):
         """ check if brain has lead image """
 
-    def type_filter(self, items, types=None):
-        """ filter given list by portal_type """
-
-    def get_issue(self, context):
-        """ return the issue obj """
-
-    def get_results_from_aggregation_sources(self, context):
-        """ return a list of result sets based on aggregation sources """
-
-
 @implementer(IENLHelperView)
 class ENLHelperView(BrowserView):
     """ View with some helper methods
@@ -43,38 +33,3 @@ class ENLHelperView(BrowserView):
                 return
             has_image = item_object.getRawImage()
         return has_image
-
-    def type_filter(self, items, types=None):
-        """ filter given list by portal_type
-        """
-        if not types:
-            return items
-        allowed_items = []
-        for item in items:
-            if item.portal_type not in types:
-                continue
-            allowed_items.append(item)
-        return allowed_items
-
-    def get_issue(self, context):
-        return api.content.get(UID=context.issue_uid)
-
-    def get_results_from_aggregation_sources(self, context):
-        """ Returns a list of sources and it's results
-        """
-        sources = context.getContentSources()
-        results = []
-        for source in sources:
-            sresults = source.queryCatalog()
-            if not sresults:
-                continue
-            results.append({
-                'id': source.id,
-                'title': source.Title(),
-                'description': source.Description(),
-                'uid': source.UID(),
-                'portal_type': sresults[0].portal_type,
-                'brains': sresults,
-                'brains_count': len(sresults),
-            })
-        return results
