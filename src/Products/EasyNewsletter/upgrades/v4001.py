3ef4e76c66c6bf26fe0b08cbe61bc06c95599b71
Marek Kralewski <mck@tuxwerk.de>
2020-06-08 20:25:11 +0200

    remove old upgrade steps

--- src/Products/EasyNewsletter/upgrades/v4001.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import log
-
-
-def upgrade(context):
-    """
-    """
-    logger.info("Running Python upgrade steps")
--- src/Products/EasyNewsletter/upgrades/v4003.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# from . import logger
-
-
-def upgrade(setup_tool=None):
-    """
-    """
-    # logger.info("Running Python upgrade steps")
